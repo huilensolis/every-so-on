8d982c6cc029d024f5fc5309e5b43a29f1baf160
Huilensolis <xywvanz@gmail.com>
2024-08-21 17:43:09 -0300

    feat: pick a wallpaper file path from the dir $HOME/wallpaper and print it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    thread::sleep,
+    time::Duration,
+};
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    run_recursively()
+}
+
+fn run_recursively() {
+    // sleep(Duration::new(3, 0)); // TODO: uncomment line when finishing debug mode
+
+    let picked_wallpaper_file_path = pick_random_wallpaper_file();
+
+    set_wallpaper(&picked_wallpaper_file_path)
+    // run_recursively(); // TODO: uncomment line when finishing debug mode
+}
+
+fn pick_random_wallpaper_file() -> std::ffi::OsString {
+    let home_path = match home::home_dir() {
+        Some(path) => {
+            if path.as_os_str().is_empty() {
+                panic!("could not find home path");
+            }
+
+            path
+        }
+        _ => panic!("could not find home path"),
+    };
+
+    let wallpaper_dir_path = home_path.as_path().join("wallpapers");
+
+    let wallpaper_files = fs::read_dir(&wallpaper_dir_path).unwrap_or_else(|error_msg| {
+        panic!(
+            "could not read wallpapers directory, directory {:?} is missing",
+            &wallpaper_dir_path.as_os_str()
+        )
+    });
+
+    let mut wallpaper_list: Vec<fs::DirEntry> = vec![];
+
+    for path_result in wallpaper_files {
+        let path = path_result.expect("path in wallpaper dir doesn't exist");
+
+        wallpaper_list.push(path);
+    }
+
+    let rand_number = rand::thread_rng().gen_range(0..wallpaper_list.len() - 1);
+
+    let picked_wallpaper_path = wallpaper_list[rand_number].path();
+
+    picked_wallpaper_path.as_os_str().to_owned()
+}
+
+fn set_wallpaper(new_wallpaper_path: &std::ffi::OsStr) {
+    println!("{:?}", &new_wallpaper_path);
 }
